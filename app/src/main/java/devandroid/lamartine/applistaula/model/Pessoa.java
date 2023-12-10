e7ec53a60889175223ef700392293d22706f3a5a
LAMARTINE LOPES <lamartinelps@gmail.com>
2023-12-10 11:54:14 -0300

    Uso dos métodos Get e Set.

--- app/src/main/java/devandroid/lamartine/applistaula/model/Pessoa.java
+++ app/src/main/java/devandroid/lamartine/applistaula/model/Pessoa.java
@@ -1,10 +1,46 @@
 package devandroid.lamartine.applistaula.model;
 
 public class Pessoa {
+    //Construtor
+    //Atributos - Objeto - Molde - Modelo - Template
+    // Metodos de acesso - Gets and Sets
 
-    public Pessoa(){
 
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
+    public Pessoa(){}
 
+    public String getPrimeiroNome() {
+        return primeiroNome;
     }
 
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
 }
--- app/src/main/java/devandroid/lamartine/applistaula/view/MainActivity.java
+++ app/src/main/java/devandroid/lamartine/applistaula/view/MainActivity.java
@@ -11,11 +11,49 @@ public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
 
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        //Atribuir conteudo, dados.
+        pessoa.setPrimeiroNome("Lamartine");
+        pessoa.setSobreNome("Lopes");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("11-88889999");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Lamartine");
+        outraPessoa.setSobreNome("Lopes");
+        outraPessoa.setCursoDesejado("Android");
+        outraPessoa.setTelefoneContato("11-88889999");
+
+        dadosPessoa = " Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = " Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+
+
+
     }
 }
